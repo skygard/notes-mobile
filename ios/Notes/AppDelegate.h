e7afed3ab7fb2375c6bc2475b8ed4d064025439d
Chris Harvey <chris@chrisnharvey.com>
2019-05-08 20:29:29 +0100

    Add window property to AppDelegate.h

--- ios/Notes/AppDelegate.h
+++ ios/Notes/AppDelegate.h
@@ -11,6 +11,7 @@
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RNAppAuthAuthorizationFlowManager>
 
+@property (nonatomic, strong) UIWindow *window;
 @property(nonatomic, weak)id<RNAppAuthAuthorizationFlowManagerDelegate>authorizationFlowManagerDelegate;
 
 @end
