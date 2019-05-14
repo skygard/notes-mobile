419cfcb874fe8ab246c9578e34b054133a787adb
Chris Harvey <chris@chrisnharvey.com>
2019-05-14 20:55:30 +0100

    Add empty NoteList component

---
+++ app/NoteList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export default class NoteList extends Component {
+  static navigationOptions = {
+    title: 'Notes'
+  }
+
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}
