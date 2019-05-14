2a2178aefe3cab3a313f5ef5295b0f3a0e3df849
Chris Harvey <chris@chrisnharvey.com>
2019-05-14 20:55:17 +0100

    Add navigation logic to App.js

--- App.js
+++ App.js
@@ -1,49 +1,42 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { createSwitchNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
+import NoteList from './app/NoteList';
+import Login from './app/Login';
+import ServerSettings from './app/ServerSettings';
+import AuthCheck from './app/AuthCheck';
+import { setNavigator } from './helpers/navigation';
+import Settings from './lib/Settings';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+// Settings.clearAll();
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+const AppStack = createStackNavigator({
+  NoteList,
 });
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+const AuthStack = createStackNavigator({
+  Login,
+  ServerSettings,
+});
+
+const AppContainer = createAppContainer(
+  createSwitchNavigator(
+    {
+      AuthCheck,
+      App: AppStack,
+      Auth: AuthStack,
+    },
+    {
+      initialRouteName: 'AuthCheck',
+    },
+  ),
+);
+
+export default function () {
+  return (
+    <AppContainer
+      ref={(navigatorRef) => {
+        setNavigator(navigatorRef);
+      }}
+    />
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
